1023bff39a3eee6b501c1a7d596df4c483de518d
Dimitrios Piliouras <jimpil1985@gmail.com>
2019-11-22 14:15:41 +0000

    rework equality

--- src/cryptohash_clj/equality.clj
+++ src/cryptohash_clj/equality.clj
@@ -1,17 +1,19 @@
 (ns cryptohash-clj.equality)
 
-(defn hash= ;; adopted from `crypto-equality`
+(defn hash= ;; adapted from `crypto-equality`
   "Test whether two sequences of characters or bytes are equal in a way that
-  protects against timing attacks. Note that this does not prevent an attacker
-  from discovering the *length* of the data being compared."
+   protects against timing attacks. Note that this does not prevent an attacker
+   from discovering the *length* of the data being compared."
   [a b]
-  (let [a (seq (map int a))
-        b (seq (map int b))]
+  (let [a (some->> a not-empty (mapv int))
+        b (some->> b not-empty (mapv int))]
     (or
       (and (nil? a)
            (nil? b))
       (and (some? a)
            (some? b)
-           (= (count a) (count b))
-           (zero? (reduce bit-or (map bit-xor a b))))
+           (= (count a)
+              (count b))
+           (zero?
+             (reduce bit-or (mapv bit-xor a b))))
       false)))
