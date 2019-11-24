20fcb24e22890a33d20f576dc0c191ea4cb88932
Dimitrios Piliouras <jimpil1985@gmail.com>
2019-11-24 14:47:38 +0000

    revert hash=

--- src/clojure/cryptohash_clj/equality.clj
+++ src/clojure/cryptohash_clj/equality.clj
@@ -2,19 +2,24 @@
 
 (defn hash= ;; adapted from `crypto-equality`
   "Test whether two sequences of characters/bytes are equal in a way that
-   protects against timing attacks. Early aborting occurs if one or both
-   arguments are nil (one => false,  both => true)."
+   protects against timing attacks. Early aborting occurs if one/both
+   arguments are nil (one => false,  both => true), or if the sequences
+   are found to be of different lengths. Note that, the latter can allow
+   an attacker to discover the length of the data being compared.
+   However, in the context of crypto-hashing this is not a concern as the
+   length of a key is typically not considered to be a secret
+   (e.g. BCrypt & the entire SHA family have well-known/fixed key lengths)."
   [a b]
-  (let [a (some->> a not-empty (mapv int))
-        b (some->> b not-empty (mapv int))]
+  (let [a (some->> a not-empty (map int))
+        b (some->> b not-empty (map int))]
     (or
       (and (nil? a)
            (nil? b)) ;; both nil => true
       (and (some? a) ;; one of them nil => false
            (some? b)
-           (->> (map bit-xor a b) ;; assume lengths match
+           (= (count a)
+              (count b)) ;; ensure equal lengths before comparison
+           (->> (map bit-xor a b)
                 (reduce bit-or)
-                zero?)
-           (= (count a) ;; confirm it in constant-time at the end
-              (count b)))
+                zero?))
       false)))
