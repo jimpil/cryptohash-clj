5ea9e4abb83065d044ea7471ed7e86b85450e300
jimpil <jimpil1985@gmail.com>
2020-12-21 22:09:59 +0000

    Version 0.1.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryptohash-clj "0.1.10"
+(defproject cryptohash-clj "0.1.11-SNAPSHOT"
   :description "Cryptographic hashing facilities (pbkdf2/bcrypt/scrypt/argon2) for Clojure"
   :url "https://github.com/jimpil/cryptohash-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
